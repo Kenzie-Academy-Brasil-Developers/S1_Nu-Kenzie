25456a26bd7fee73d5f646715edea02e8e60800a
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-01-25 14:36:24 -0300

    feat: structuring react base

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Form from './components/Form'
-import TotalMoney from './components/TotalMoney'
-import List from './components/List'
-import Header from './components/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Form from "./components/Form";
+import TotalMoney from "./components/TotalMoney";
+import List from "./components/List";
+import Header from "./components/Header";
 
 function App() {
   const [listTransactions, setListTransactions] = useState([
     { description: "Salário recebido", type: "entrada", value: 2500 },
-    { description: "Conta de luz", type: "saída", value: -150 }
-])
+    { description: "Conta de luz", type: "saída", value: -150 },
+  ]);
 
   return (
     <>
-      <Header/>
+      <Header />
       <aside>
-        <Form listTransactions={listTransactions} setListTransactions={setListTransactions}/>
-        <TotalMoney listTransactions={listTransactions}/>
+        <Form
+          listTransactions={listTransactions}
+          setListTransactions={setListTransactions}
+        />
+        <TotalMoney listTransactions={listTransactions} />
       </aside>
       <main>
-        <List listTransactions={listTransactions}/>
+        <List listTransactions={listTransactions} />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
